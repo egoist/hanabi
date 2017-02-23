a583ae1ef56cda30e67a597a35cca0c3b3f51cd9
EGOIST <0x142857@gmail.com>
2017-02-23 20:34:38 +0800

    Updates

---
+++ 1.a74c81fe.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,6],{14:function(n,o){n.exports='import { h, render } from \'preact\';\n\nrender((\n    <div id="foo">\n        <span>Hello, world!</span>\n        <button onClick={ e => alert("hi!") }>Click Me</button>\n    </div>\n), document.body);\n'}});
+//# sourceMappingURL=1.a74c81fe.js.map
