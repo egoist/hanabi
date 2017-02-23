74f30799c9b7ee13683ed91ea5f4337a62bf45f4
EGOIST <0x142857@gmail.com>
2017-02-23 17:57:06 +0800

    update test

--- test/index.test.js
+++ test/index.test.js
@@ -2,5 +2,5 @@ import hanabi from '../src'
 
 test('main', () => {
   const out = hanabi(`co`)
-  expect(out).toBe('<span style="color: #23AC69">c</span><span style="color: #91C132">o</span>')
+  expect(out).toBe('<span style=\"color: #23AC69\">co</span>')
 })
