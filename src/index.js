57e77c9da1ce62609f528d3ed1eb5ffeb7a32379
EGOIST <0x142857@gmail.com>
2017-02-23 16:39:06 +0800

    fix lint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const defaultColors = ['23AC69','91C132','F19726','E8552D','1AAB8E','E1147F','2980C1','1BA1E6','9FA0A0','F19726','E30B20','E30B20','A3338B']
+const defaultColors = ['23AC69', '91C132', 'F19726', 'E8552D', '1AAB8E', 'E1147F', '2980C1', '1BA1E6', '9FA0A0', 'F19726', 'E30B20', 'E30B20', 'A3338B']
 
 export default function (input, {
   colors = defaultColors
--- test/index.test.js
+++ test/index.test.js
@@ -2,5 +2,5 @@ import hanabi from '../src'
 
 test('main', () => {
   const out = hanabi(`co`)
-  expect(out).toBe('<span style=\"color: #91C132\">c</span><span style=\"color: #F19726\">o</span>')
+  expect(out).toBe('<span style="color: #91C132">c</span><span style="color: #F19726">o</span>')
 })
